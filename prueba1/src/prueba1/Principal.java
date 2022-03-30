7627d7de73c7faec7c3af4f6f3ae5cf91f778f20
Usuario2 <fatimarodriguez@iesmarquesdecomares.org>
2022-03-30 10:01:10 +0200

    modifico la clase Principal

--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -5,6 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo java-git");
 		System.out.println("Añado segunda línea");
+		System.out.println("Añado rama cambio1");
 
 	}
 
